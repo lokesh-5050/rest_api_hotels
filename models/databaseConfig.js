b0b6beab384211aaa2c55c38e5435058a2c47614
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2023-04-02 16:05:40 +0530

    db added

--- models/databaseConfig.js
+++ models/databaseConfig.js
@@ -1,17 +1,16 @@
-var mysql = require('mysql2');
+var mysql = require("mysql2");
 var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'hotelsmanager',
-    user: 'root',
-    password: 'root123'
+  host: process.env.DB_URL,
+  database: "sql9610435",
+  user: "sql9610435",
+  password: process.env.DB_PASS,
 });
 connection.connect(function (err) {
-    if (err) {
-        console.error(`Error connecting: + err.stack`)
-        return
-    }
-
-})
+  if (err) {
+    console.error(`Error connecting: + err.stack`);
+    return;
+  }
+});
 // connection.query('SELECT * FROM users', function (error, results, fields) {
 //     if (error)
 //         throw error;
